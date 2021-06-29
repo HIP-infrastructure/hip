104c423999ef359aaac8c2d15703214cecef3e91
Manuel Spuhler <manuel.spuhler@gmail.com>
2021-06-29 15:46:48 +0200

    Changed static path

--- templates/index.php
+++ templates/index.php
@@ -1,8 +1,8 @@
 <div id="hip-root" style="width: 100%;"></div>
 
-<?php style($_['appId'], '../static/css/2.6ceee308.chunk') ?>
-<?php style($_['appId'], '../static/css/main.5c4678c0.chunk') ?>
+<?php style($_['appId'], '../build/css/2.6ceee308.chunk') ?>
+<?php style($_['appId'], '../build/css/main.5c4678c0.chunk') ?>
 
-<?php script($_['appId'], '../static/js/main'); ?>
-<?php script($_['appId'], '../static/js/2.2eb794ff.chunk'); ?>
-<?php script($_['appId'], '../static/js/main.2b2cb7a8.chunk'); ?>
+<?php script($_['appId'], '../build/js/main'); ?>
+<?php script($_['appId'], '../build/js/2.2eb794ff.chunk'); ?>
+<?php script($_['appId'], '../build/js/main.2b2cb7a8.chunk'); ?>
