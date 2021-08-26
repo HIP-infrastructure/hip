4dbf0d2ee0f6f17c20b406ecdbe14e73afc2e132
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-08-26 16:23:34 +0000

    Build test #2

--- templates/index.prod.php
+++ templates/index.prod.php
@@ -1,1 +1,4 @@
-<?php style($_['appId'], '../dist/build/static/js/bundle.min') ?>
+<div id="hip-root" style="width: 100%;"></div>
+
+<?php script($_['appId'], '../dist/build/static/js/bundle.min') ?>
+<?php script($_['appId'], '../dist/build/static/js/main'); ?>
