c11cdb2574a1e99f06a5f80e2d399a13580c7e0a
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-08-27 13:52:03 +0000

    Prod buil

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,7 @@
 <div id="hip-root" style="width: 100%;"></div>
-
-<?php script($_['appId'], '../static/js/bundle'); ?>
-<?php script($_['appId'], '../static/js/vendors~main.chunk'); ?>
-<?php script($_['appId'], '../static/js/main.chunk'); ?>
+<?php script($_['appId'], '../build/static/js/2.7d090710.chunk'); ?>
+<?php script($_['appId'], '../build/static/js/3.fc882f11.chunk'); ?>
+<?php script($_['appId'], '../build/static/js/main.28db781a.chunk'); ?>
+<?php script($_['appId'], '../build/static/js/runtime-main.69d7b504'); ?>
+<?php style($_['appId'], '../build/static/css/2.987415ef.chunk'); ?>
+<?php style($_['appId'], '../build/static/css/main.d60ec860.chunk'); ?>
