a1a9da6d1b9039b9fa6ea7277b6fde49ad439114
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-09-03 07:10:53 +0000

    build fixes

--- buildhipapp.sh
+++ buildhipapp.sh
@@ -6,11 +6,15 @@ echo '<div id="hip-root" style="width: 100%;"></div>' > $TEMPLATE
 
 JS=$(ls build/static/js/*.js | xargs -n 1 basename)
 for f in $JS; do 
-    echo "<?php script(\$_['appId'], '../build/static/js/${f%.*}'); ?>" >> $TEMPLATE
+    echo "<?php script(\$_['appId'], '../static/js/${f%.*}'); ?>" >> $TEMPLATE
 done
 
 CSS=$(ls build/static/css/*.css | xargs -n 1 basename)
 for f in $CSS; do 
-    echo "<?php style(\$_['appId'], '../build/static/css/${f%.*}'); ?>" >> $TEMPLATE
+    echo "<?php style(\$_['appId'], '../static/css/${f%.*}'); ?>" >> $TEMPLATE
 done
+
+rm -rf static
+mv build/static static
+rm -rf build
 
