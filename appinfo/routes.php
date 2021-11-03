c6a01a82f89bdc7d22e45d1fdc7f4a97febc2f30
Manuel Spuhler <manuel.spuhler@chuv.ch>
2021-11-03 15:20:33 +0000

    set/unset tags, send to broker

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,4 +3,5 @@
 return ['routes' => [
     ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
     ['name' => 'document#list', 'url' => '/document/list', 'verb' => 'GET'],
-]];
+    ['name' => 'tag#list', 'url' => '/tag/list', 'verb' => 'GET']
+    ]];
--- lib/Listener/TagEventListener.php
+++ lib/Listener/TagEventListener.php
@@ -5,24 +5,50 @@ namespace OCA\HIP\Listener;
 use OCP\EventDispatcher\IEventListener;
 use OCP\EventDispatcher\Event;
 use OCP\SystemTag\MapperEvent;
+use OC\Files\Filesystem;
 use OCA\HIP\Service\MessageService;
-use OCP\ILogger;
+use Psr\Log\LoggerInterface;
+use Throwable;
+use function sprintf;
 
-class TagEventListener implements IEventListener {
+class TagEventListener implements IEventListener
+{
+    private $logger;
 
-    public function __construct(ILogger $logger)
-	{
-		$this->logger = $logger;
-	}
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
-    public function handle(Event $event): void {
-        if (!($event instanceOf MapperEvent)) {
-        //     return;
+    public function handle(Event $event): void
+    {
+        if ($event instanceof MapperEvent) {
+            //     return;
+
+            try {
+                $this->logger->debug(
+                    sprintf('TagEventListener %d', $event->getEvent())
+                );
+
+                $objectId = $event->getObjectId();
+                $path = Filesystem::getPath($objectId);
+                $info = Filesystem::getFileInfo($path);
+                // $a = \OC::$server->getRootFolder()->getById($objectId);
+
+                $message = array();
+                $message['event'] = $event->getEvent();
+                $message['objectId'] = $objectId;
+                $message['path'] = $path;
+                // $message['info'] = $info;
+
+                $messageService = new MessageService();
+                $messageService->send($path);
+                // $messageService->send(serialize($message));
+            } catch (Throwable $e) {
+                $this->logger->error('Error sending event: ' . $e->getMessage(), [
+                    'exception' => $e,
+                ]);
+            }
         }
-
-		$this->logger->info($event->getEvent());
-
-        $message = new MessageService();
-		$message->send($event->getEvent());
     }
 }
