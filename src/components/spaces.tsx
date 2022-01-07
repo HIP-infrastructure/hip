7cf1c85077996ec4ce484094b74c7a006b35f830
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-01-07 15:52:01 +0000

    Cleanup wip

--- src/components/spaces.tsx
+++ src/components/spaces.tsx
@@ -1,43 +1,27 @@
 import React from 'react'
 import Sessions from './sessions'
 import Apps from './apps'
-import WebdavForm from './webdavLoginForm'
-import { useAppStore } from '../store/appProvider'
 import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material';
 
-const Spaces = (): JSX.Element => {
-	const {
-		showWedavForm: [showWedavForm, setShowWedavForm],
-	} = useAppStore()
+const Spaces = (): JSX.Element => <Box
+	sx={{
+		display: 'flex',
+	}}>
 
-	return (
-		<Box
-			sx={{
-				display: 'flex',
-			}}>
-			{/* <Dialog
-				header='Data access'
-				visible={showWedavForm}
-				onHide={() => setShowWedavForm(false)}
-			>
-				<WebdavForm />
-			</Dialog> */}
-			<Outlet />
-			<Box>
-				<h2>Applications</h2>
-				<Apps />
-			</Box>
-			<Box
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					minWidth: '960px'
-				}}>
-				<Sessions />
-			</Box>
-		</Box >
-	)
-}
+	<Outlet />
+	<Box>
+		<h2>Applications</h2>
+		<Apps />
+	</Box>
+	<Box
+		sx={{
+			display: 'flex',
+			flexDirection: 'column',
+			minWidth: '960px'
+		}}>
+		<Sessions />
+	</Box>
+</Box >
 
 export default Spaces
