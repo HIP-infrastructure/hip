df143d2aa0136d32df3b464fba8e03cb3b39c6c2
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-01-07 15:52:53 +0000

    Apply theme

--- src/components/sessionInfo.tsx
+++ src/components/sessionInfo.tsx
@@ -4,10 +4,14 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Container } from '../api/gatewayClientAPI';
 import { DRAWER_WIDTH } from '../constants'
+import { useTheme } from '@mui/material/styles';
 
 const SessionInfo = ({ session }: { session?: Container }) => {
+    const theme = useTheme();
     return (
-        <Card sx={{ minWidth: DRAWER_WIDTH }}>
+        <Card sx={{ minWidth: DRAWER_WIDTH,
+            backgroundColor: theme.palette.primary.light
+        }}>
             <CardContent>
                 <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
                     {session?.user}
@@ -19,6 +23,7 @@ const SessionInfo = ({ session }: { session?: Container }) => {
                     {session?.state}
                 </Typography>
                 <Typography variant="body2">
+                    <a href={session?.url}>Direct link</a> 
                     {session?.error}
                 </Typography>
             </CardContent>
