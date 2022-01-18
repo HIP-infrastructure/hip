131d7139f69c54d88f3575ede4828faab826bea9
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-01-18 12:22:25 +0000

    Session layout

--- src/components/sessionInfo.tsx
+++ src/components/sessionInfo.tsx
@@ -4,26 +4,25 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Container } from '../api/gatewayClientAPI';
 import { DRAWER_WIDTH } from '../constants'
-import { useTheme } from '@mui/material/styles';
 
 const SessionInfo = ({ session }: { session?: Container }) => {
-    const theme = useTheme();
+
     return (
-        <Card sx={{ minWidth: DRAWER_WIDTH,
-            backgroundColor: theme.palette.primary.light
+        <Card sx={{
+            minWidth: DRAWER_WIDTH,
         }}>
             <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                <Typography sx={{ fontSize: 14 }} gutterBottom>
                     {session?.user}
                 </Typography>
                 <Typography variant="h5" component="div">
                     Session #{session?.name}
                 </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                <Typography sx={{ mb: 1.5 }}>
                     {session?.state}
                 </Typography>
                 <Typography variant="body2">
-                    <a href={session?.url}>Direct link</a> 
+                    <a href={session?.url}>Direct link</a>
                     {session?.error}
                 </Typography>
             </CardContent>
