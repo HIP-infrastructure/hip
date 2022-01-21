1a1ee97a3fe785e19c607b4f5ec7aeb4abc8981d
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-01-21 14:51:39 +0000

    php fixes

--- templates/index.dev.php
+++ templates/index.dev.php
@@ -1,5 +1,3 @@
 <div id="hip-root" style="width: 100%;"></div>
 
 <?php script($_['appId'], '../static/js/bundle'); ?>
-<?php script($_['appId'], '../static/js/vendors~main.chunk'); ?>
-<?php script($_['appId'], '../static/js/main.chunk'); ?>
