2f7fa16acb87dfafc56686c33d90f8dfe5f72ab5
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-02-13 18:04:54 +0000

    Fixed casting

--- src/components/apps.tsx
+++ src/components/apps.tsx
@@ -68,7 +68,7 @@ const Apps = () => {
 	}
 
 	const modalStyle = {
-		position: 'absolute' as 'absolute',
+		position: 'absolute' as const,
 		top: '50%',
 		left: '50%',
 		transform: 'translate(-50%, -50%)',
