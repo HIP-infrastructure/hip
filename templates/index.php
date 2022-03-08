b22be85d34fd0b7264ed9087b391f8b5c61f5289
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-03-08 17:19:56 +0000

    Build 0.0.4

--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,3 @@
 <div id="hip-root" style="width: 100%;"></div>
-<?php script($_['appId'], '../static/js/main.248a024f'); ?>
+<?php script($_['appId'], '../static/js/main.a5ad38ee'); ?>
 <?php style($_['appId'], '../static/css/main.1a822487'); ?>
