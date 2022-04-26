99c297f4ee2deab6f68e97571b00cfe0823ea55b
Manuel Spuhler <manuel.spuhler@gmail.com>
2022-04-26 18:54:18 +0200

    Fixed bids data list

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -1,9 +1,8 @@
 import { Alert, Box, CircularProgress, Link, Typography } from '@mui/material'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { getBids, getFiles } from '../api/gatewayClientAPI'
-import { BIDSDatabaseResponse, TreeNode } from '../api/types'
+import { getBids } from '../api/bids'
+import { BIDSDatabase, BIDSDatabaseResponse } from '../api/types'
 import TitleBar from './UI/titleBar'
 
 // interface ExtendedBIDSDatabase extends BIDSDatabase {
@@ -16,27 +15,27 @@ const columns: GridColDef[] = [
 		headerName: 'Name',
 		width: 320,
 	},
-	{
-		field: 'Participants',
-		headerName: 'Participants',
-		type: 'number',
-		sortable: false,
-		width: 160,
-	},
-	{
-		field: 'Browse',
-		headerName: 'Browse',
-		sortable: false,
-		renderCell: (params: { value: string }) => (
-			<Link
-				target='_blank'
-				href={`${window.location.protocol}//${window.location.host}/apps/files/?dir=${params.value}`}
-			>
-				Browse
-			</Link>
-		),
-		width: 150,
-	},
+	// {
+	// 	field: 'Participants',
+	// 	headerName: 'Participants',
+	// 	type: 'number',
+	// 	sortable: false,
+	// 	width: 160,
+	// },
+	// {
+	// 	field: 'Browse',
+	// 	headerName: 'Browse',
+	// 	sortable: false,
+	// 	renderCell: (params: { value: string }) => (
+	// 		<Link
+	// 			target='_blank'
+	// 			href={`${window.location.protocol}//${window.location.host}/apps/files/?dir=${params.value}`}
+	// 		>
+	// 			Browse
+	// 		</Link>
+	// 	),
+	// 	width: 150,
+	// },
 	{
 		field: 'Authors',
 		headerName: 'Authors',
@@ -50,65 +49,61 @@ const columns: GridColDef[] = [
 		headerName: 'BIDS Version',
 		width: 150,
 	},
-	{
-		field: 'Path',
-		headerName: 'Path',
-		sortable: false,
-		width: 320,
-	},
+	// {
+	// 	field: 'Path',
+	// 	headerName: 'Path',
+	// 	sortable: false,
+	// 	width: 320,
+	// },
 ]
 
 // https://hip.local/apps/files/?dir=
 
 const Data = (): JSX.Element => {
-	const [bidsDatabase, setBidsDatabase] = useState<BIDSDatabaseResponse>()
-	const [filesPanes, setFilesPanes] = useState<TreeNode[][]>()
+	const [bidsDatabase, setBidsDatabase] = useState<BIDSDatabase[]>()
+	const [error, setError] = useState<Error | null>()
 	const [ignored, forceUpdate] = React.useReducer(x => x + 1, 0)
-	const navigate = useNavigate()
+
 
 	useEffect(() => {
-		getBids().then(r => {
-			setBidsDatabase(r)
+		getBids().then((r: BIDSDatabaseResponse) => {
+
+			if (r.error) {
+				setError(error)
+				return
+			}
+
+			setBidsDatabase(r.data)
 		})
 		// files('/').then(f => setFilesPanes([f]))
 	}, [])
 
-	const files = async (path: string) => {
-		return await getFiles(path)
-	}
+	// const files = async (path: string) => {
+	// 	return await getFiles(path)
+	// }
 
-	const handleSelectedPath = async (pathes: string[]) => {
-		const path = pathes.join('')
-		const result = await files(path)
-		setFilesPanes(prev => {
-			if (!prev) return [result]
+	// const handleSelectedPath = async (pathes: string[]) => {
+	// 	const path = pathes.join('')
+	// 	const result = await files(path)
+	// 	setFilesPanes(prev => {
+	// 		if (!prev) return [result]
 
-			prev[pathes.length - 1] = result
-			prev.splice(pathes.length)
+	// 		prev[pathes.length - 1] = result
+	// 		prev.splice(pathes.length)
 
-			return prev
-		})
-		forceUpdate()
-	}
+	// 		return prev
+	// 	})
+	// 	forceUpdate()
+	// }
 
-	const rows =
-		bidsDatabase?.data?.map(db => ({
-			id: db.Path,
-			Name: db.Name,
-			Authors: db.Authors,
-			Participants: db.Participants && db.Participants.length,
-			Licence: db.Licence,
-			BIDSVersion: db.BIDSVersion,
-			Path: db.Path,
-			Browse: db.Path,
-		})) || []
+
 
 	return (
 		<>
 			<TitleBar title='Data' />
 
-			{bidsDatabase?.error && (
-				<Alert severity='error'>{bidsDatabase.error.message}</Alert>
+			{error && (
+				<Alert severity='error'>{error.message}</Alert>
 			)}
 
 			<Box sx={{ mt: 2 }}>
@@ -138,14 +133,15 @@ const Data = (): JSX.Element => {
 			<Box sx={{ mt: 2 }}>
 				<Typography variant='h6'>
 					BIDS Databases{' '}
-					{!bidsDatabase?.data && !bidsDatabase?.error && (
+					{!bidsDatabase && !error && (
 						<CircularProgress size={16} />
 					)}
 				</Typography>
 
 				<Box sx={{ height: 500, width: '100%' }}>
 					<DataGrid
-						rows={rows}
+						getRowId={(params) => params.Name}
+						rows={bidsDatabase || []}
 						columns={columns}
 						pageSize={100}
 						rowsPerPageOptions={[100]}
