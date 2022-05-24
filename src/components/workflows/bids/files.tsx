28dce8e280137e08ff70209f80ff71dc4001b1a0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-05-24 18:49:52 +0200

    Entities WIP

--- src/components/workflows/bids/files.tsx
+++ src/components/workflows/bids/files.tsx
@@ -2,37 +2,29 @@ import { Article, Delete, Folder, Save } from '@mui/icons-material'
 import { LoadingButton } from '@mui/lab'
 import {
 	Autocomplete,
-	Box, Grid,
+	Box,
+	Grid,
 	IconButton,
 	MenuItem,
-	Paper, Table,
+	Paper,
+	Table,
 	TableBody,
 	TableCell,
 	TableContainer,
 	TableHead,
 	TableRow,
-	TextField
+	TextField,
+	Typography,
 } from '@mui/material'
 import { Form, Formik } from 'formik'
 import React, { useEffect, useState } from 'react'
 import * as Yup from 'yup'
+import { getSubject } from '../../../api/bids'
 import { getFiles } from '../../../api/gatewayClientAPI'
-import {
-	BidsDatabaseDefinitionDto, File, TreeNode
-} from '../../../api/types'
+import { File, TreeNode } from '../../../api/types'
 import { ENTITIES, MODALITIES } from '../../../constants'
 import { useNotification } from '../../../hooks/useNotification'
 import { useAppStore } from '../../../store/appProvider'
-
-const validationSchema = Yup.object().shape({
-	subject: Yup.string().required('Subject is required'),
-	modality: Yup.string().required('Modality is required'),
-})
-
-const initialValues = {
-	subject: '',
-	modality: '',
-}
 
 const Files = (): JSX.Element => {
 	const [ignored, forceUpdate] = React.useReducer((x: number) => x + 1, 0)
@@ -41,8 +33,10 @@ const Files = (): JSX.Element => {
 	const [inputValue, setInputValue] = React.useState<string>('')
 	const [submitted, setSubmitted] = useState(false)
 	const { showNotif } = useNotification()
-	const [definitions, setDefinitions] = useState<BidsDatabaseDefinitionDto>()
 	const [currentBidsFile, setCurrentBidsFile] = useState<File>()
+	const [modality, setModality] = useState<{ name: string; type: string }>()
+	const [selectedSubject, setSelectedSubject] = useState<string>()
+	const [entities, setEntites] = useState<any>()
 
 	const {
 		containers: [containers],
@@ -61,6 +55,44 @@ const Files = (): JSX.Element => {
 	}, [])
 
 	useEffect(() => {
+		if (
+			!(selectedBidsDatabase?.path && user?.uid && selectedSubject && modality)
+		)
+			return
+
+		const subject = selectedSubject.replace('sub-', '')
+		const entitiesByModality =
+			(modality &&
+				ENTITIES.filter(e =>
+					e.requirements.map(r => r.dataType).includes(modality?.type)
+				)) ||
+			[]
+		getSubject(selectedBidsDatabase?.path, user?.uid, subject)
+			.then(d => {
+				const nextEntities = entitiesByModality?.map(e => {
+					const entries = d
+						.filter(i => i.modality === modality?.name)
+						.find(eem => {
+							return Object.keys(eem).find(k => k === e.name)
+						})
+					const mod = (entries as Record<string, any>)[e.name]
+
+					if (mod)
+						return {
+							...e,
+							modalities: Array.from(new Set([...e.modalities, mod])),
+						}
+					else return e
+				})
+				if (nextEntities) setEntites(nextEntities)
+				else setEntites(entitiesByModality)
+			})
+			.catch(e => {
+				console.log(e)
+			})
+	}, [selectedSubject, modality])
+
+	useEffect(() => {
 		// console.log(tree?.map((t: { data: { path: any } }) => t.data.path))
 		const selectedNode = tree?.find(
 			(node: { data: { path: any } }) => node.data.path === inputValue
@@ -77,21 +109,21 @@ const Files = (): JSX.Element => {
 			...(parentNode
 				? [parentNode]
 				: [
-					{
-						key: 'root',
-						label: '../',
-						icon: 'dir',
-						data: {
-							path: '/',
-							type: 'dir',
-							size: 0,
-							updated: 'string',
-							name: '../',
-							tags: [],
-							id: 0,
+						{
+							key: 'root',
+							label: '../',
+							icon: 'dir',
+							data: {
+								path: '/',
+								type: 'dir',
+								size: 0,
+								updated: 'string',
+								name: '../',
+								tags: [],
+								id: 0,
+							},
 						},
-					},
-				]),
+				  ]),
 			...(tree
 				?.filter((node: { data: { path: string } }) =>
 					new RegExp(inputValue).test(node.data.path)
@@ -153,9 +185,27 @@ const Files = (): JSX.Element => {
 		const nextFiles = selectedFiles.filter(f => f.path !== file.path)
 	}
 
+	const validationSchema = Yup.object().shape({
+		subject: Yup.string().required('Subject is required'),
+		modality: Yup.string().required('Modality is required'),
+	})
+
+	const initialValues = {
+		subject: '',
+		modality: '',
+		entities: [],
+	}
+
 	return (
-		<Box sx={{ display: 'flex' }}>
-			<Box sx={{ flex: '1 1'}}>
+		<Grid container spacing={2}>
+			<Grid item xs={8}>
+				<Typography
+					sx={{ mt: 1, mb: 2 }}
+					variant='body2'
+					color='text.secondary'
+				>
+					Select modalities, entities and files to be imported
+				</Typography>
 				<Formik
 					initialValues={initialValues}
 					validationSchema={validationSchema}
@@ -174,23 +224,23 @@ const Files = (): JSX.Element => {
 							setSelectedParticipants(s => [...(s || []), participant])
 						}
 
-						const file: File = {
-							modality: values.modality,
-							subject: values.subject.replace('sub-', ''),
-							path: currentBidsFile?.path?.substring(1),
-							entities: {
-								sub: values.subject.replace('sub-', ''),
-								...ENTITIES?.reduce(
-									(a, k) => ({
-										...a,
-										...((values as any)[k] ? { [k]: (values as any)[k] } : {}),
-									}),
-									{}
-								),
-							},
-						}
+						// const file: File = {
+						// 	modality: values.modality,
+						// 	subject: values.subject.replace('sub-', ''),
+						// 	path: currentBidsFile?.path?.substring(1),
+						// 	entities: {
+						// 		sub: values.subject.replace('sub-', ''),
+						// 		...ENTITIES?.reduce(
+						// 			(a, k) => ({
+						// 				...a,
+						// 				...((values as any)[k] ? { [k]: (values as any)[k] } : {}),
+						// 			}),
+						// 			{}
+						// 		),
+						// 	},
+						// }
 
-						setSelectedFiles(f => [...(f || []), file])
+						// setSelectedFiles(f => [...(f || []), file])
 
 						// resetForm()
 						// showNotif('File added.', 'success')
@@ -198,11 +248,17 @@ const Files = (): JSX.Element => {
 						setSubmitted(false)
 					}}
 				>
-					{({ errors, handleChange, touched, values }) => (
+					{({ errors, handleChange, handleBlur, touched, values }) => (
 						<Form>
-							<Grid container columnSpacing={2} rowSpacing={2}>
+							<Box
+								sx={{
+									display: 'flex',
+									flexDirection: 'column',
+									gap: '0.8em 0.8em',
+								}}
+							>
 								{participants && (
-									<Grid item xs={6}>
+									<Box>
 										<TextField
 											fullWidth
 											select
@@ -211,12 +267,16 @@ const Files = (): JSX.Element => {
 											name='subject'
 											label='Subject'
 											value={values.subject}
-											onChange={handleChange}
+											onChange={event => {
+												setSelectedSubject(event.target.value)
+												handleChange(event)
+											}}
+											onBlur={handleBlur}
 											error={touched.subject && errors.subject ? true : false}
 											helperText={
 												touched.subject && errors.subject
 													? errors.subject
-													: null
+													: 'Select a subject to import files for'
 											}
 										>
 											{participants?.map(p => (
@@ -228,123 +288,191 @@ const Files = (): JSX.Element => {
 												</MenuItem>
 											))}
 										</TextField>
-									</Grid>
+									</Box>
 								)}
-
-								<Grid item xs={6}>
-									<TextField
-										fullWidth
-										select
-										size='small'
-										disabled={submitted}
-										name='modality'
-										label='Modality'
-										value={values.modality}
-										onChange={handleChange}
-										error={touched.modality && errors.modality ? true : false}
-										helperText={
-											touched.modality && errors.modality
-												? errors.modality
-												: null
-										}
-									>
-										{MODALITIES?.map(m => (
-											<MenuItem value={m}>{m}</MenuItem>
-										))}
-									</TextField>
-								</Grid>
-
-								{ENTITIES.map(k => (
-									<Grid item xs={2} key={k}>
+								{values.subject && (
+									<Box>
 										<TextField
+											fullWidth
+											select
 											size='small'
 											disabled={submitted}
-											name={k}
-											label={k}
-											value={(values as Record<string, string>)[k]}
-											onChange={handleChange}
-											error={
-												(touched as Record<string, string>)[k] &&
-													(errors as Record<string, string>)[k]
-													? true
-													: false
-											}
+											name='modality'
+											label='Modality'
+											value={values.modality}
+											onChange={event => {
+												const m = MODALITIES.find(
+													m => m.name === event.target.value
+												)
+												if (m) setModality(m)
+												handleChange(event)
+											}}
+											onBlur={handleBlur}
+											error={touched.modality && errors.modality ? true : false}
 											helperText={
-												(touched as Record<string, string>)[k] &&
-													(errors as Record<string, string>)[k]
-													? (errors as Record<string, string>)[k]
+												touched.modality && errors.modality
+													? errors.modality
 													: null
 											}
-										/>
-									</Grid>
-								))}
-								<Grid item xs={10}>
-									<Autocomplete
-										options={options || []}
-										inputValue={inputValue}
-										onInputChange={(event: any, newInputValue: string) => {
-											handleSelectedPath(newInputValue)
-											setInputValue(newInputValue)
-										}}
-										disableCloseOnSelect={true} // tree?.find(node => node.data.path === inputValue)?.data.type !== 'file'}
-										id='input-tree-view'
-										renderInput={(params: unknown) => (
-											<TextField {...params} label='Files' />
-										)}
-										renderOption={(props, option) => {
-											const node = tree?.find(node => node.data.path === option)
-
-											return node?.data.type === 'dir' ? (
-												<Box
-													component='li'
-													sx={{ '& > svg': { mr: 1, flexShrink: 0 } }}
-													{...props}
-												>
-													<Folder color='action' />
-													{option}
-												</Box>
-											) : (
-												<Box
-													component='li'
-													sx={{ '& > svg': { mr: 1, flexShrink: 0 } }}
-													{...props}
-												>
-													<Article color='action' />
-													{option}
-												</Box>
-											)
-										}}
-									/>
-								</Grid>
-								<Grid item xs={2}>
-									<LoadingButton
-										color='primary'
-										type='submit'
-										loading={submitted}
-										loadingPosition='start'
-										startIcon={<Save />}
-										variant='contained'
+										>
+											{MODALITIES?.map(m => (
+												<MenuItem value={m.name} key={m.name}>
+													{m.name}
+												</MenuItem>
+											))}
+										</TextField>
+									</Box>
+								)}
+								{values.modality && (
+									<Typography
+										sx={{ mt: 1, mb: 1 }}
+										variant='body2'
+										color='text.secondary'
 									>
-										Add
-									</LoadingButton>
-								</Grid>
-							</Grid>
+										BIDS entities
+									</Typography>
+								)}
+								{values.modality && (
+									<Box
+										sx={{
+											display: 'flex',
+											gap: '0.8em 0.8em',
+											flexWrap: 'wrap',
+										}}
+									>
+										{entities?.map(entity => (
+											<Box
+												key={entity.name}
+												sx={{
+													maxWidth: '200px',
+													flex: 'inherit',
+												}}
+											>
+												<TextField
+													size='small'
+													select
+													disabled={submitted}
+													name={entity.name}
+													label={entity.label}
+													value={(values as Record<string, string>)[entity]}
+													onChange={handleChange}
+													onBlur={handleBlur}
+													error={
+														(touched as Record<string, string>)[entity] &&
+														(errors as Record<string, string>)[entity]
+															? true
+															: false
+													}
+													helperText={
+														(touched as Record<string, string>)[entity] &&
+														(errors as Record<string, string>)[entity]
+															? (errors as Record<string, string>)[entity]
+															: entity.description
+													}
+												>
+													{entity?.modalities?.map(m => (
+														<MenuItem key={m} value={m}>
+															{m}
+														</MenuItem>
+													))}
+												</TextField>
+											</Box>
+										))}
+									</Box>
+								)}
+								{values.entities && (
+									<Box>
+										<Typography
+											sx={{ mt: 1, mb: 1 }}
+											variant='body2'
+											color='text.secondary'
+										>
+											Select files to be imported
+										</Typography>
+									</Box>
+								)}{' '}
+								{values.entities && (
+									<Box
+										sx={{
+											display: 'flex',
+											gap: '0.8em 0.8em',
+										}}
+									>
+										<Autocomplete
+											sx={{ flexGrow: 1 }}
+											options={options || []}
+											inputValue={inputValue}
+											onInputChange={(event: any, newInputValue: string) => {
+												handleSelectedPath(newInputValue)
+												setInputValue(newInputValue)
+											}}
+											disableCloseOnSelect={true} // tree?.find(node => node.data.path === inputValue)?.data.type !== 'file'}
+											id='input-tree-view'
+											renderInput={(params: unknown) => (
+												<TextField {...params} label='Files' />
+											)}
+											renderOption={(props, option) => {
+												const node = tree?.find(
+													node => node.data.path === option
+												)
+
+												return node?.data.type === 'dir' ? (
+													<Box
+														component='li'
+														sx={{ '& > svg': { mr: 1, flexShrink: 0 } }}
+														{...props}
+													>
+														<Folder color='action' />
+														{option}
+													</Box>
+												) : (
+													<Box
+														component='li'
+														sx={{ '& > svg': { mr: 1, flexShrink: 0 } }}
+														{...props}
+													>
+														<Article color='action' />
+														{option}
+													</Box>
+												)
+											}}
+										/>
+										<LoadingButton
+											color='primary'
+											type='submit'
+											size='small'
+											loading={submitted}
+											loadingPosition='start'
+											startIcon={<Save />}
+											variant='contained'
+										>
+											Add
+										</LoadingButton>
+									</Box>
+								)}
+							</Box>
 						</Form>
 					)}
 				</Formik>
-			</Box>
-			<Box sx={{ flex: '1 1'}}>
+			</Grid>
+			<Grid item xs={4}>
+				<Typography
+					sx={{ mt: 1, mb: 2 }}
+					variant='body2'
+					color='text.secondary'
+				>
+					Files to be imported
+				</Typography>
 				<TableContainer component={Paper}>
 					<Table size='small' aria-label='simple table'>
 						<TableHead>
 							<TableRow>
 								<TableCell>Actions</TableCell>
 								<TableCell>Subject</TableCell>
-								<TableCell>Modality</TableCell>
-								<TableCell>Path</TableCell>
-								{ENTITIES?.map((k: string) => (
+								<TableCell>Info</TableCell>
+								{/* {ENTITIES?.map((k: string) => (
 									<TableCell key={k}>{k}</TableCell>
-								))}
+								))} */}
 							</TableRow>
 						</TableHead>
 						<TableBody>
@@ -364,19 +492,19 @@ const Files = (): JSX.Element => {
 									<TableCell>{file.subject}</TableCell>
 									<TableCell>{file.modality}</TableCell>
 									<TableCell>{file.path}</TableCell>
-									{file.entities &&
+									{/* {file.entities &&
 										ENTITIES.map(k => (
 											<TableCell key={k}>
 												{file.entities ? file.entities[k] : ''}
 											</TableCell>
-										))}
+										))} */}
 								</TableRow>
 							))}
 						</TableBody>
 					</Table>
 				</TableContainer>
-			</Box>
-		</Box>
+			</Grid>
+		</Grid>
 	)
 }
 
