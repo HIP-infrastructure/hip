67e3f274cd55db2b2dceb0af882672e2e9210f7a
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-05-31 16:54:26 +0200

    Summary table

--- src/components/workflows/bids/summary.tsx
+++ src/components/workflows/bids/summary.tsx
@@ -1,4 +1,5 @@
-import { Box, CircularProgress, Typography } from '@mui/material'
+import { Delete } from '@mui/icons-material'
+import { Box, CircularProgress, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import React from 'react'
 import { useAppStore } from '../../../store/appProvider'
 
@@ -12,7 +13,6 @@ const Summary = ({ completed }: { completed: boolean }): JSX.Element => {
 		selectedFiles: [selectedFiles, setSelectedFiles],
 	} = useAppStore()
 
-
 	return (
 		<Box>
 			<Box>
@@ -21,6 +21,37 @@ const Summary = ({ completed }: { completed: boolean }): JSX.Element => {
 				</Typography>
 
 				{!completed && <CircularProgress size={16} />}
+
+				<Typography
+					sx={{ mt: 1, mb: 2 }}
+					variant='subtitle1'
+					color='text.secondary'
+				>
+					Imported files
+				</Typography>
+				<TableContainer component={Paper}>
+					<Table size='small' aria-label='simple table'>
+						<TableHead>
+							<TableRow>
+								<TableCell>Subject</TableCell>
+								<TableCell>Modality</TableCell>
+								<TableCell>Path</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{selectedFiles?.reverse().map(file => (
+								<TableRow
+									key={file.path}
+									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+									<TableCell>{file.subject}</TableCell>
+									<TableCell>{file.modality}</TableCell>
+									<TableCell>{file.path}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
 			</Box>
 		</Box>
 	)
