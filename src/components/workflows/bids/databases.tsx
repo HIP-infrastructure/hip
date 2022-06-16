17532777839a4402510cb702a6a69838f4780aea
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-06-16 15:28:35 +0200

    Reworked databases

--- src/components/workflows/bids/databases.tsx
+++ src/components/workflows/bids/databases.tsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import DataGrid from 'react-data-grid'
-
 import { Add } from '@mui/icons-material'
 import {
 	Alert,
 	Box,
 	Button,
+	Card,
+	CardActions,
+	CardContent,
 	CircularProgress,
 	Link,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
 	Typography,
 } from '@mui/material'
 import Checkbox from '@mui/material/Checkbox'
-
 import { getBidsDatabases } from '../../../api/bids'
 import { BIDSDatabase } from '../../../api/types'
 import { useAppStore } from '../../../store/appProvider'
 import CreateDatabase from './forms/CreateDatabase'
-import { useMatomo } from '@jonkoops/matomo-tracker-react'
 
 const Databases = (): JSX.Element => {
-	const { trackEvent } = useMatomo()
-
 	const [rows, setRows] = useState<BIDSDatabase[]>([])
 	const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false)
 	const [dataBaseCreated, setDatabaseCreated] = useState(false)
@@ -59,151 +63,166 @@ const Databases = (): JSX.Element => {
 		}
 	}, [dataBaseCreated])
 
-	function rowKeyGetter(row: BIDSDatabase) {
-		return row.id
-	}
-
-	const columns = [
-		{
-			key: 'select-row',
-			name: '',
-			minWidth: 50,
-			maxWidth: 50,
-			resizable: false,
-			sortable: false,
-			frozen: true,
-			formatter: ({
-				row,
-				isCellSelected,
-			}: {
-				row: BIDSDatabase
-				isCellSelected: boolean
-			}) => {
-				if (isCellSelected) {
-					setSelectedBidsDatabase(row)
-					trackEvent({ 
-						category: 'bids', 
-						action: 'select-database' 
-					})
-				}
-
-				const isSelected = isCellSelected || selectedBidsDatabase?.id === row.id
-
-				return <Checkbox checked={isSelected} />
-			},
-		},
-		{
-			key: 'Name',
-			name: 'Name',
-			resizable: true,
-			frozen: true,
-			width: 200,
-		},
-		{
-			key: 'participants',
-			name: 'Participants',
-			formatter(props: any) {
-				return <>{props.row.participants.length}</>
-			},
-		},
-		{
-			key: 'Authors',
-			name: 'Authors',
-			resizable: true,
-		},
-		{
-			key: 'BIDSVersion',
-			name: 'Version',
-		},
-		{
-			key: 'path',
-			name: 'Path',
-			resizable: true,
-			formatter: (props: any) => {
-				return (
-					<Link
-						target='_blank'
-						href={`${window.location.protocol}//${window.location.host}/apps/files/?dir=${props.row.path}`}
-					>
-						{props.row.path}
-					</Link>
-				)
-			},
-		},
-		{
-			key: 'Licence',
-			name: 'Licence',
-			resizable: true,
-		},
-		{
-			key: 'Acknowledgements',
-			name: 'Acknowledgements',
-			resizable: true,
-		},
-		{
-			key: 'HowToAcknowledge',
-			name: 'How To Acknowledge',
-			resizable: true,
-		},
-		{
-			key: 'Funding',
-			name: 'Funding',
-			resizable: true,
-		},
-		{
-			key: 'ReferencesAndLinks',
-			name: 'References And Links',
-			resizable: true,
-		},
-		{
-			key: 'DatasetDOI',
-			name: 'datasetDOI',
-			resizable: true,
-		},
-	]
-
 	return (
 		<>
-			<Box sx={{ mt: 2 }}>
-				<Box
-					sx={{
-						display: 'flex',
-						justifyContent: 'space-between',
-						alignItems: 'center',
-					}}
-				>
-					<Typography variant='h6'>
-						BIDS Databases {!bidsDatabases && <CircularProgress size={16} />}
-					</Typography>
-					{bidsDatabases?.error && (
-						<Alert severity='error'>{bidsDatabases?.error.message}</Alert>
-					)}
+			<CreateDatabase
+				open={isCreateDialogOpen}
+				handleClose={() => setIsCreateDialogOpen(!isCreateDialogOpen)}
+				setDatabaseCreated={setDatabaseCreated}
+			/>
+			<Box
+				sx={{
+					display: 'flex',
+					gap: '1em',
+					width: 'inherit',
+					mr: 2,
+				}}
+			>
+				<Paper sx={{ width: 'calc(100vw - 640px - 240px)' }}>
+					<TableContainer sx={{ maxHeight: 440 }}>
+						<Table stickyHeader size='small' aria-label='BIDS Databases tables'>
+							<TableHead>
+								<TableRow>
+									<TableCell></TableCell>
+									<TableCell sx={{ width: 320 }}>Name</TableCell>
+									<TableCell>Participants</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{rows.map(row => (
+									<TableRow
+										hover
+										role='checkbox'
+										tabIndex={-1}
+										key={row.id}
+										onClick={() => setSelectedBidsDatabase(row)}
+									>
+										<TableCell padding='checkbox'>
+											<Checkbox
+												color='primary'
+												checked={row.id === selectedBidsDatabase?.id}
+												onChange={() => {
+													setSelectedBidsDatabase(row)
+												}}
+												inputProps={{
+													'aria-label': 'select database',
+												}}
+											/>
+										</TableCell>
+										<TableCell>{row.Name}</TableCell>
+										<TableCell>{row.participants?.length}</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+					<Box
+						sx={{
+							display: 'flex',
+							justifyContent: 'space-between',
+							alignItems: 'center',
+						}}
+					>
+						{!bidsDatabases && <CircularProgress sx={{ m: 2 }} size={16} />}
+
+						{bidsDatabases?.error && (
+							<Alert sx={{ m: 2 }} severity='error'>
+								{bidsDatabases?.error.message}
+							</Alert>
+						)}
+					</Box>
 					<Button
 						color='primary'
 						size='small'
-						sx={{ mt: 0.5, mb: 2 }}
+						sx={{ m: 2 }}
 						startIcon={<Add />}
 						onClick={() => setIsCreateDialogOpen(true)}
 						variant={'contained'}
 					>
 						Create BIDS Database
 					</Button>
-				</Box>
-
-				<DataGrid
-					rowClass={row =>
-						row.id === selectedBidsDatabase?.id ? 'selected-row' : ''
-					}
-					columns={columns}
-					rows={rows}
-					rowKeyGetter={rowKeyGetter}
-				/>
+				</Paper>
+				<Card sx={{ width: 480 }}>
+					{selectedBidsDatabase && (
+						<>
+							<CardContent>
+								<Typography gutterBottom variant='h6' component='div'>
+									{selectedBidsDatabase?.Name}
+								</Typography>
+								<Typography sx={{ mb: 1.5 }} color='text.secondary'>
+									{selectedBidsDatabase?.Authors?.toString()}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>Version</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{selectedBidsDatabase?.BIDSVersion}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>Licence</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.Licence &&
+										selectedBidsDatabase.Licence) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>Acknowledgements</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.Acknowledgements &&
+										selectedBidsDatabase.Acknowledgements) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>How To Acknowledge</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.HowToAcknowledge &&
+										selectedBidsDatabase.HowToAcknowledge) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>Funding</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.Funding &&
+										selectedBidsDatabase.Funding.toString()) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>References and Links</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.ReferencesAndLinks &&
+										selectedBidsDatabase.ReferencesAndLinks.toString()) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>DOI</strong>
+								</Typography>
+								<Typography gutterBottom variant='body1'>
+									{(selectedBidsDatabase?.DatasetDOI &&
+										selectedBidsDatabase.DatasetDOI) ||
+										'n/a'}
+								</Typography>
+								<Typography variant='body1'>
+									<strong>Files</strong>
+								</Typography>
+								<Box>
+									<Link
+										target='_blank'
+										href={`${window.location.protocol}//${window.location.host}/apps/files/?dir=${selectedBidsDatabase?.path}`}
+									>
+										{selectedBidsDatabase?.path}
+									</Link>
+								</Box>
+							</CardContent>
+							<CardActions></CardActions>
+						</>
+					)}
+				</Card>
 			</Box>
-			<CreateDatabase
-				open={isCreateDialogOpen}
-				handleClose={() => setIsCreateDialogOpen(!isCreateDialogOpen)}
-				setDatabaseCreated={setDatabaseCreated}
-			/>
 		</>
 	)
 }
