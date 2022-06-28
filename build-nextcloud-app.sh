8719420ba9a8a920e3a15f6aa830b7e6f6195377
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-06-28 18:01:33 +0200

    Release script fixes

--- build-nextcloud-app.sh
+++ build-nextcloud-app.sh
@@ -8,12 +8,12 @@ TEMPLATE="$TARGET/templates/index.php"
 
 echo '<div id="hip-root" style="width: 100%;"></div>' > $TEMPLATE
 
-JS=$(ls $TARGET//static/js/*.js | xargs -n 1 basename)
+JS=$(ls $TARGET/static/js/*.js | xargs -n 1 basename)
 for f in $JS; do 
     echo "<?php script(\$_['appId'], '../static/js/${f%.*}'); ?>" >> $TEMPLATE
 done
 
-CSS=$(ls $TARGET//static/css/*.css | xargs -n 1 basename)
+CSS=$(ls $TARGET/static/css/*.css | xargs -n 1 basename)
 for f in $CSS; do 
     echo "<?php style(\$_['appId'], '../static/css/${f%.*}'); ?>" >> $TEMPLATE
 done
--- release.sh
+++ release.sh
@@ -11,7 +11,7 @@ git pull origin master
 git diff --quiet --exit-code
 [ $? != 0 ] && echo There is unstaged changes && exit 1
 
-[ -z "$GITHUB_TOKEN" ] && echo GITHUB_TOKEN var is missing. Go to https://github.com/settings/tokens get one and put it in .credentials && exit 1
+[ -z "$GITHUB_TOKEN" ] && echo GITHUB_TOKEN var is missing. Go to https://github.com/settings/tokens && exit 1
 
 VERSION=v`grep '<version>' appinfo/info.xml | sed 's/[^0-9.]//g'`
 UPLOAD_URL=`curl -sH "Authorization: token $GITHUB_TOKEN" -d "{\"tag_name\":\"$VERSION\"}" https://api.github.com/repos/$GITHUB_REPO/releases | grep '"upload_url"' | sed 's/.*"\(https:.*\){.*/\1/'`
