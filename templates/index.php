493d219705290f4536703bdfffafbf27139ccbcd
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-06-28 15:38:06 +0200

    Cleanup build files

--- templates/index.php
+++ templates/index.php
@@ -1,3 +1,3 @@
 <div id="hip-root" style="width: 100%;"></div>
-<?php script($_['appId'], '../static/js/main.640d6019'); ?>
-<?php style($_['appId'], '../static/css/main.1a822487'); ?>
+
+<?php script($_['appId'], '../static/js/bundle'); ?>
