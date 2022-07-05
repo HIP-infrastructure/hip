b6cd3b6700394e5cb6508a840ca394c436daee3b
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-07-05 11:30:01 +0200

    Theme color

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,13 +1,27 @@
 import * as React from 'react'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
+// See https://material.io/resources/color/#!/?view.left=0&view.right=0&primary.color=37474f&secondary.color=0277BD
+
+const colors = {
+	primary: '#0277bd',
+	secondary: '#37474f',
+}
+
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: '#0095a8',
+			main: colors.primary,
 		},
 		secondary: {
-			main: '#37474f',
+			main: colors.secondary,
+		},
+	},
+	components: {
+		MuiButton: {
+			defaultProps: {
+				disableRipple: true,
+			},
 		},
 	},
 })
