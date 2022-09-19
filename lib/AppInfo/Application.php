df1c6180b4bc19d917d9843fb8ab86f33462aad0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-09-19 18:44:17 +0200

    feat(php): groupfolders api

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,24 +6,21 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\EventDispatcher\IEventDispatcher;
-use OCP\SystemTag\MapperEvent;
-use OCA\HIP\Listener\TagEventListener;
 
-
-class Application extends App implements IBootstrap {
+class Application extends App implements IBootstrap
+{
 	public const APP_ID = 'hip';
 
-	public function __construct() {
+	public function __construct()
+	{
 		parent::__construct(self::APP_ID);
-
-		$dispatcher = $this->getContainer()->query(IEventDispatcher::class);
-		$dispatcher->addServiceListener(MapperEvent::EVENT_ASSIGN, TagEventListener::class); 
 	}
 
-	public function register(IRegistrationContext $context): void {
+	public function register(IRegistrationContext $context): void
+	{
 	}
 
-	public function boot(IBootContext $context): void {
+	public function boot(IBootContext $context): void
+	{
 	}
 }
