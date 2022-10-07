923e85fd85c450b8fc7cd1b1c5e60be79e5de3a0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-10-07 13:53:20 +0200

    refactor: isloggedin api in NC app

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -3,41 +3,32 @@
 namespace OCA\HIP\Controller;
 
 use OCP\AppFramework\Controller;
-use OCP\AppFramework\Http\DataResponse;
-use OCA\GroupFolders\Folder\FolderManager;
-use OCP\Files\IRootFolder;
 use OCP\IRequest;
 use OCP\ILogger;
+use OCP\IUserSession;
 
 class ApiController extends Controller
 {
-    private FolderManager $manager;
-    private IRootFolder $rootFolder;
-
     public function __construct(
         ILogger $logger,
         string $AppName,
         IRequest $request,
-        FolderManager $manager,
-        IRootFolder $rootFolder,
+        IUserSession $userSession
     ) {
         parent::__construct($AppName, $request);
+
         $this->logger = $logger;
         $this->request = $request;
-        $this->manager = $manager;
-        $this->rootFolder = $rootFolder;
+        $this->userSession = $userSession;
     }
 
-    /**
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     */
-    public function groupfolders()
+    public function isloggedin()
     {
-        return new DataResponse($this->manager->getAllFoldersWithSize($this->getRootFolderStorageId()));
+        return $this->userSession->isLoggedIn();
     }
 
-    private function getRootFolderStorageId(): int {
-		return $this->rootFolder->getMountPoint()->getNumericStorageId();
-	}
+    public function uid()
+    {
+        return $this->userSession->getUser()->getUID();
+    }
 }
