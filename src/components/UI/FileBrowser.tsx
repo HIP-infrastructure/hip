777076b274aece25a4eb48137ea0fab2888e45ae
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-11-03 15:33:07 +0100

    fix: key error in file array

--- src/components/UI/FileBrowser.tsx
+++ src/components/UI/FileBrowser.tsx
@@ -8,37 +8,26 @@ import { getFiles2, getGroupFolders, search } from '../../api/gatewayClientAPI'
 import { File2, ISearch } from '../../api/types'
 import { useAppStore } from '../../store/appProvider'
 
-function MinusSquare(props: SvgIconProps) {
-	return (
-		<SvgIcon fontSize='inherit' style={{ width: 14, height: 14 }} {...props}>
-			{/* tslint:disable-next-line: max-line-length */}
-			<path d='M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z' />
-		</SvgIcon>
-	)
-}
+const MinusSquare = (props: SvgIconProps) => (
+	<SvgIcon fontSize='inherit' style={{ width: 14, height: 14 }} {...props}>
+		{/* tslint:disable-next-line: max-line-length */}
+		<path d='M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 11.023h-11.826q-.375 0-.669.281t-.294.682v0q0 .401.294 .682t.669.281h11.826q.375 0 .669-.281t.294-.682v0q0-.401-.294-.682t-.669-.281z' />
+	</SvgIcon>
+)
 
-function PlusSquare(props: SvgIconProps) {
-	return (
-		<SvgIcon fontSize='inherit' style={{ width: 14, height: 14 }} {...props}>
-			{/* tslint:disable-next-line: max-line-length */}
-			<path d='M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z' />
-		</SvgIcon>
-	)
-}
+const PlusSquare = (props: SvgIconProps) => (
+	<SvgIcon fontSize='inherit' style={{ width: 14, height: 14 }} {...props}>
+		{/* tslint:disable-next-line: max-line-length */}
+		<path d='M22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0zM17.873 12.977h-4.923v4.896q0 .401-.281.682t-.682.281v0q-.375 0-.669-.281t-.294-.682v-4.896h-4.923q-.401 0-.682-.294t-.281-.669v0q0-.401.281-.682t.682-.281h4.923v-4.896q0-.401.294-.682t.669-.281v0q.401 0 .682.281t.281.682v4.896h4.923q.401 0 .682.281t.281.682v0q0 .375-.281.669t-.682.294z' />
+	</SvgIcon>
+)
 
-function CloseSquare(props: SvgIconProps) {
-	return (
-		<SvgIcon
-			className='close'
-			fontSize='inherit'
-			style={{ width: 14, height: 14 }}
-			{...props}
-		>
-			{/* tslint:disable-next-line: max-line-length */}
-			<path d='M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z' />
-		</SvgIcon>
-	)
-}
+const CloseSquare = (props: SvgIconProps) => (
+	<SvgIcon fontSize='inherit' style={{ width: 14, height: 14 }} {...props}>
+		{/* tslint:disable-next-line: max-line-length */}
+		<path d='M17.485 17.512q-.281.281-.682.281t-.696-.268l-4.12-4.147-4.12 4.147q-.294.268-.696.268t-.682-.281-.281-.682.294-.669l4.12-4.147-4.12-4.147q-.294-.268-.294-.669t.281-.682.682-.281.696 .268l4.12 4.147 4.12-4.147q.294-.268.696-.268t.682.281 .281.669-.294.682l-4.12 4.147 4.12 4.147q.294.268 .294.669t-.281.682zM22.047 22.074v0 0-20.147 0h-20.12v0 20.147 0h20.12zM22.047 24h-20.12q-.803 0-1.365-.562t-.562-1.365v-20.147q0-.776.562-1.351t1.365-.575h20.147q.776 0 1.351.575t.575 1.351v20.147q0 .803-.575 1.365t-1.378.562v0z' />
+	</SvgIcon>
+)
 
 const StyledTreeItem = styled((props: TreeItemProps) => (
 	<TreeItem {...props} />
@@ -157,34 +146,35 @@ const FileBrowser = ({
 	const sortFile = (data: File2[]) =>
 		data.sort((a: File2, b: File2) => -b.name.localeCompare(a.name))
 
-	const renderLabel = (item: File2) => {
+	const renderLabel = (file: File2) => {
 		return (
-			(files.find(i => i.parentPath === item.path) && (
-				<span>{item.name}</span>
+			// file exists, don't load it again
+			(files.find(i => i.parentPath === file.path) && (
+				<span>{file.name}</span>
 			)) || (
 				<span
 					onClick={event => {
 						event.stopPropagation()
 						event.preventDefault()
 
-						if (item.isDirectory) {
-							getFiles2(item.path)
+						if (file.isDirectory) {
+							getFiles2(file.path)
 								.then(data => setFiles(f => [...f, ...data]))
 								.then(() => {
-									setExpanded(items => [...items, item.path])
+									setExpanded(items => [...items, file.path])
 								})
 						} else {
-							selectedFile && selectedFile(item.path)
+							selectedFile && selectedFile(file.path)
 						}
 					}}
 				>
-					{item.name}
+					{file.name}
 				</span>
 			)
 		)
 	}
 
-	const subItems = (file: File2) => {
+	const subFiles = (file: File2) => {
 		const items = files
 			?.filter(f => new RegExp(file.path).test(f.parentPath || ''))
 			?.filter(f => {
@@ -195,12 +185,12 @@ const FileBrowser = ({
 				return false
 			})
 			.map(f => (
-				<StyledTreeItem key={file.path} label={renderLabel(f)} nodeId={f.path}>
-					{subItems(f)}
+				<StyledTreeItem key={f.path} label={renderLabel(f)} nodeId={f.path}>
+					{subFiles(f)}
 				</StyledTreeItem>
 			))
 
-		// Display a fake item to show the expand icon
+		// At root: display a fake item to show the expand icon
 		return file.isDirectory ? (
 			items.length > 0 ? (
 				items
@@ -264,7 +254,7 @@ const FileBrowser = ({
 							label={renderLabel(file)}
 							nodeId={file.path}
 						>
-							{subItems(file)}
+							{subFiles(file)}
 						</StyledTreeItem>
 					))}
 			</TreeView>
