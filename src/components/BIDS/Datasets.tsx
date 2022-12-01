f5d54995c0389967a61d7e4fba8fc744c677dc7e
Manuel Spuhler <manuel.spuhler@chuv.ch>
2022-12-01 15:24:07 +0100

    refactor: debounce query ranges

--- src/components/BIDS/Datasets.tsx
+++ src/components/BIDS/Datasets.tsx
@@ -19,15 +19,16 @@ import {
 	useTheme,
 } from '@mui/material'
 import React, { useCallback, useEffect, useState } from 'react'
-import { refreshBidsDatasetsIndex, queryBidsDatasets } from '../../api/bids'
+import { queryBidsDatasets, refreshBidsDatasetsIndex } from '../../api/bids'
 import { BIDSDataset } from '../../api/types'
+import useDebounce from '../../hooks/useDebounce'
 import { useAppStore } from '../../store/appProvider'
 import TitleBar from '../UI/titleBar'
 import CreateDataset from './CreateDataset'
 import DatasetCard from './DatasetCard'
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
+const ITEM_HEIGHT = 48
+const ITEM_PADDING_TOP = 8
 const DatatypeMenuProps = {
 	PaperProps: {
 		style: {
@@ -35,15 +36,46 @@ const DatatypeMenuProps = {
 			width: 250,
 		},
 	},
-};
+}
+
+const datatypes = [
+	'anat',
+	'dwi',
+	'func',
+	'ieeg',
+	'eeg',
+	//'fmap',
+	//'pet',
+	//'ct',
+]
+
+const participantsCountRangeMarks = [
+	{ value: 0, label: '0' },
+	{ value: 200, label: '200+' },
+]
+
+const ageRangeMarks = [
+	{ value: 0, label: '0' },
+	{ value: 100, label: '100' },
+]
 
 const Datasets = () => {
+	const theme = useTheme()
+	const {
+		user: [user],
+	} = useAppStore()
+
 	const [isCreateDialogOpen, setIsCreateDialogOpen] = useState(false)
 	const [datasetCreated, setDatasetCreated] = useState(false)
 	const [term, setTerm] = useState('*')
-	const [ageRange, setAgeRange] = useState<number[]>([0, 100])
-	const [participantsCountRange, setParticipantsCountRange] = useState<number[]>([0, 200]);
-
+	const [debouncedAgeRange, ageRange, setAgeRange] = useDebounce<number[]>([
+		0, 100,
+	])
+	const [
+		debouncedParticipantsCount,
+		participantsCountRange,
+		setParticipantsCountRange,
+	] = useDebounce<number[]>([0, 200])
 	const [numberOfResultsPerPage, setNumberOfResultsPerPage] =
 		useState<number>(20)
 	const [totalNumberOfDatasets, setTotalNumberOfDatasets] = useState<number>(0)
@@ -51,82 +83,35 @@ const Datasets = () => {
 	const [datasets, setDatasets] = useState<
 		{ data?: BIDSDataset[]; error?: string } | undefined
 	>()
-
-	const {
-		user: [user],
-	} = useAppStore()
-
-	const participantsCountRangeMarks = [
-		{
-			value: 0,
-			label: '0',
-		},
-		{
-			value: 200,
-			label: '200+',
-		},
-	]
-
-	const ageRangeMarks = [
-		{
-			value: 0,
-			label: '0',
-		},
-		{
-			value: 100,
-			label: '100',
-		},
-	]
-
-	const datatypes = [
-		'anat',
-		'dwi',
-		'func',
-		'ieeg',
-		'eeg',
-		//'fmap',
-		//'pet',
-		//'ct',
-	]
-	const theme = useTheme();
-	const [datatype, setDatatype] = React.useState<string[]>(datatypes);
-  
-	const handleDatatypeChange = (event: SelectChangeEvent<typeof datatype>) => {
-		const {
-			target: { value },
-		} = event
-		setDatatype(
-			// On autofill we get a stringified value.
-			typeof value === 'string' ? value.split(',') : value,
-		)
-	}
-
-	const handleAgeRangeChange = (event: Event, newValue: number | number[]) => {
-		setAgeRange(newValue as number[])
-	}
-
-	const handleParticipantsCountRangeChange = (event: Event, newValue: number | number[]) => {
-		setParticipantsCountRange(newValue as number[])
-	}
-  
-	function getStyles(name: string, datatype: readonly string[], theme: Theme) {
-		return {
-			fontWeight:
-			datatype.indexOf(name) === -1
-				? theme.typography.fontWeightRegular
-				: theme.typography.fontWeightMedium,
-		}
-	}
+	const [datatype, setDatatype] = React.useState<string[]>(datatypes)
 
 	const queryDatasets = useCallback(async () => {
-		queryBidsDatasets(user?.uid, term, page, numberOfResultsPerPage, ageRange, participantsCountRange, datatype)
+		setDatasets(undefined)
+		queryBidsDatasets(
+			user?.uid,
+			term,
+			page,
+			numberOfResultsPerPage,
+			debouncedAgeRange,
+			debouncedParticipantsCount,
+			datatype
+		)
 			.then(data => {
 				setDatasets({ data })
 			})
 			.catch(error => {
 				setDatasets({ error })
 			})
-	}, [queryBidsDatasets, user, term, ageRange, participantsCountRange, datatype, page, numberOfResultsPerPage])
+	}, [
+		queryBidsDatasets,
+		user,
+		term,
+		debouncedAgeRange,
+		debouncedParticipantsCount,
+		datatype,
+		page,
+		numberOfResultsPerPage,
+	])
 
 	useEffect(() => {
 		queryDatasets()
@@ -141,6 +126,25 @@ const Datasets = () => {
 			setDatasetCreated(false)
 		}
 	}, [queryDatasets, datasetCreated, user?.uid])
+
+	const handleDatatypeChange = (event: SelectChangeEvent<typeof datatype>) => {
+		const {
+			target: { value },
+		} = event
+		setDatatype(
+			// On autofill we get a stringified value.
+			typeof value === 'string' ? value.split(',') : value
+		)
+	}
+
+	function getStyles(name: string, datatype: readonly string[], theme: Theme) {
+		return {
+			fontWeight:
+				datatype.indexOf(name) === -1
+					? theme.typography.fontWeightRegular
+					: theme.typography.fontWeightMedium,
+		}
+	}
 
 	return (
 		<>
@@ -169,14 +173,6 @@ const Datasets = () => {
 			/>
 
 			<Box sx={{ mt: 2 }}>
-				{!datasets && (
-					<CircularProgress
-						size={32}
-						color='secondary'
-						sx={{ top: 10, left: 10 }}
-					/>
-				)}
-
 				{datasets?.error && <Alert severity='error'>{datasets?.error}</Alert>}
 
 				<Box display='flex' alignItems='center' sx={{ mb: 2 }}>
@@ -208,64 +204,73 @@ const Datasets = () => {
 					</FormControl>
 				</Box>
 
-				<Box display="flex" flexDirection="row">
+				<Box display='flex' flexDirection='row'>
 					<Box sx={{ width: 250 }} mx={2}>
 						<FormControl sx={{ m: 1, minWidth: 250, maxWidth: 250 }}>
-							<InputLabel id="datatype-label">Datatypes</InputLabel>
+							<InputLabel id='datatype-label'>Datatypes</InputLabel>
 							<Select
-								labelId="datatype-label"
-								id="datatype"
+								labelId='datatype-label'
+								id='datatype'
 								multiple
 								value={datatype}
 								onChange={handleDatatypeChange}
-								input={<OutlinedInput id="datatype" label="Datatypes" />}
-								renderValue={(selected) => (
+								input={<OutlinedInput id='datatype' label='Datatypes' />}
+								renderValue={selected => (
 									<Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-									{selected.map((value) => (
-										<Chip key={value} label={value} />
-									))}
+										{selected.map(value => (
+											<Chip key={value} label={value} />
+										))}
 									</Box>
 								)}
 								MenuProps={DatatypeMenuProps}
-								>
-								{datatypes.map((dt) => (
+							>
+								{datatypes.map(dt => (
 									<MenuItem
-									key={dt}
-									value={dt}
-									style={getStyles(dt, datatype, theme)}
+										key={dt}
+										value={dt}
+										style={getStyles(dt, datatype, theme)}
 									>
-									{dt}
+										{dt}
 									</MenuItem>
 								))}
 							</Select>
 						</FormControl>
 						<Box sx={{ width: 250 }} mx={2}>
-							<Typography id="input-slider" gutterBottom>
+							<Typography id='input-slider' gutterBottom>
 								Age of participants
 							</Typography>
 							<Slider
 								getAriaLabel={() => ''}
 								value={ageRange}
-								onChange={handleAgeRangeChange}
-								valueLabelDisplay="auto"
+								onChange={(event, value) => setAgeRange(value as number[])}
+								valueLabelDisplay='auto'
 								marks={ageRangeMarks}
 							/>
 						</Box>
 						<Box sx={{ width: 250 }} mx={2}>
-							<Typography id="input-slider" gutterBottom>
+							<Typography id='input-slider' gutterBottom>
 								Number of participants
 							</Typography>
 							<Slider
 								getAriaLabel={() => 'Number of participants'}
 								value={participantsCountRange}
-								onChange={handleParticipantsCountRangeChange}
-								valueLabelDisplay="auto"
+								onChange={(event, value) =>
+									setParticipantsCountRange(value as number[])
+								}
+								valueLabelDisplay='auto'
 								marks={participantsCountRangeMarks}
 							/>
 						</Box>
 					</Box>
-				
+
 					<Box display='flex' alignItems='center' sx={{ mb: 2 }}>
+						{!datasets && (
+							<CircularProgress
+								size={32}
+								color='secondary'
+								sx={{ top: 10, left: 10 }}
+							/>
+						)}
 						<Box display='flex' justifyContent='center' alignItems='center'>
 							<Pagination
 								count={Math.ceil(
