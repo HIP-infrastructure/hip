f4b832d7264cc5e649a533a2bad6c6095ae3e247
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-04-18 16:31:01 +0200

    chore: layout

--- src/components/Center/Files.tsx
+++ src/components/Center/Files.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
+import TitleBar from '../UI/titleBar'
 
 export default function Files() {
 	const iFrameRef = useRef<HTMLIFrameElement>(null)
@@ -7,6 +8,7 @@ export default function Files() {
 	useEffect(() => {
 		if (!iFrameRef.current) return
 
+		iFrameRef?.current?.classList.add('iframe-hidden')
 		iFrameRef.current.addEventListener('load', function () {
 			const doc = iFrameRef?.current?.contentDocument
 			if (!doc) return
@@ -14,22 +16,25 @@ export default function Files() {
 			const style = doc.createElement('style')
 			style.textContent = `#header {display: none !important} #app-navigation {display: none !important}`
 			doc.head.append(style)
+			iFrameRef?.current?.classList.remove('iframe-hidden')
 		})
 	}, [iFrameRef])
 
 	return (
 		<div>
-			Files
+			<TitleBar
+				title={'Files'} />
 			<iframe
 				ref={iFrameRef}
 				title='Nextcloud Files'
 				src={'/apps/files'}
 				allow={'autoplay; clipboard-write;'}
 				style={{
-					width: 'calc(100vw - 320px)',
+					width: 'calc(50vw)',
 					height: 'calc(100vh - 100px)',
 					backgroundColor: '#333',
 				}}
+				className='iframe-display'
 			/>
 		</div>
 	)
