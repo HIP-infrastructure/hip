bbce1be3a6d82be76bbd72ebec31f083033c88de
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-04-19 15:39:29 +0200

    fix: loading for nc files

--- src/components/Center/Files.tsx
+++ src/components/Center/Files.tsx
@@ -1,7 +1,9 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import TitleBar from '../UI/titleBar'
+import { Box, CircularProgress, Typography } from '@mui/material'
 
 export default function Files() {
+	const [loading, setLoading] = useState(true)
 	const iFrameRef = useRef<HTMLIFrameElement>(null)
 
 	// Check for XPra readiness
@@ -17,25 +19,34 @@ export default function Files() {
 			style.textContent = `#header {display: none !important} #app-navigation {display: none !important}`
 			doc.head.append(style)
 			iFrameRef?.current?.classList.remove('iframe-hidden')
+			setLoading(false)
 		})
 	}, [iFrameRef])
 
 	return (
-		<div>
-			<TitleBar
-				title={'Files'} />
+		<Box>
+			<Box sx={{ mb: 2 }}>
+				<TitleBar title={'Files'} />
+			</Box>
+
+			<Typography
+			>
+				Nextcloud files{' '}
+				{loading && <CircularProgress size={18} color='secondary' />}
+			</Typography>
+
 			<iframe
 				ref={iFrameRef}
 				title='Nextcloud Files'
 				src={'/apps/files'}
 				allow={'autoplay; clipboard-write;'}
 				style={{
-					width: '50vw',
+					width: '55vw',
 					height: 'calc(100vh - 100px)',
 					backgroundColor: '#333',
 				}}
 				className='iframe-display'
 			/>
-		</div>
+		</Box>
 	)
 }
