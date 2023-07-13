3530451ccd7f3f1dfb8b0b749b0f3fe51e7f7aa0
Manuel Spuhler <manuel.spuhler@chuv.ch>
2023-07-13 16:35:34 +0200

    feat: action button for public datasets

--- src/components/Public/Datasets.tsx
+++ src/components/Public/Datasets.tsx
@@ -1,8 +1,54 @@
 import * as React from 'react'
 import Datasets from '../UI/BIDS/Datasets'
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogContentText,
+	DialogTitle,
+} from '@mui/material'
 
 const PublicDatasets = () => {
-  return <><Datasets /></>
+	const [open, setOpen] = React.useState(false)
+
+	const handleClickOpen = () => {
+		setOpen(true)
+	}
+
+	const handleClose = () => {
+		setOpen(false)
+	}
+
+	return (
+		<>
+			<Datasets
+				action={
+					<Button
+						size='small'
+						onClick={async e => {
+							e.preventDefault()
+							handleClickOpen()
+						}}
+					>
+						Clone
+					</Button>
+				}
+			/>
+			<Dialog open={open} onClose={handleClose}>
+				<DialogTitle>Clone Dataset</DialogTitle>
+				<DialogContent>
+					<DialogContentText>
+						This dataset is going to be cloned in your personal space.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleClose}>Cancel</Button>
+					<Button onClick={handleClose}>Clone</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	)
 }
 
 export default PublicDatasets
